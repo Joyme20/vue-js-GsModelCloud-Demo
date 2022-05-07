bd300e020d33068e85588b4de6a6d064a64349bf
刘卓明 <1596280876@qq.com>
2022-05-07 15:30:38 +0800

    fix

--- vue.config.js
+++ vue.config.js
@@ -3,4 +3,9 @@ module.exports = {
     assetsDir: "static",
     lintOnSave: true,
     productionSourceMap: false,
+    publicPath: "./",
+    configureWebpack: {
+        devtool: "source-map",
+        plugins: [],
+    },
 }
